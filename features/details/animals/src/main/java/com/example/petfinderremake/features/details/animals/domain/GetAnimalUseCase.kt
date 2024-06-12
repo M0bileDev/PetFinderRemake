239fba8fc498502053af43172bf274d0209706d1
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-12 19:46:19 +0200

    Methods return type was changed from Kotlin's Flow to RxJava's Observable.

--- features/details/animals/src/main/java/com/example/petfinderremake/features/details/animals/domain/GetAnimalUseCase.kt
+++ features/details/animals/src/main/java/com/example/petfinderremake/features/details/animals/domain/GetAnimalUseCase.kt
@@ -4,15 +4,14 @@ import com.example.petfinderremake.common.domain.model.animal.details.AnimalWith
 import com.example.petfinderremake.common.domain.repositories.AnimalRepository
 import com.example.petfinderremake.common.domain.result.NotYetDefinedError
 import com.example.petfinderremake.common.domain.result.Result
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
 class GetAnimalUseCase @Inject constructor(
     private val animalRepository: AnimalRepository
 ) {
 
-    operator fun invoke(id: Long): Flow<Result<AnimalWithDetails, NotYetDefinedError>> {
+    operator fun invoke(id: Long): Observable<Result<AnimalWithDetails, NotYetDefinedError>> {
         return animalRepository.getAnimal(id).map { animal ->
             Result.Success(animal)
         }
