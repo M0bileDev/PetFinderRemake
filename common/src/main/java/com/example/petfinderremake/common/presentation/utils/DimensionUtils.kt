4e867da68e7fb6938b6afc28c3a0396213d4d6c5
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-12 19:45:51 +0200

    Deleted;

--- common/src/main/java/com/example/petfinderremake/common/presentation/utils/DimensionUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.petfinderremake.common.presentation.utils
-
-import android.content.Context
-
-fun convertDpToPixels(context: Context, dp: Int) =
-    (dp * context.resources.displayMetrics.density).toInt()
-
-fun convertPixelsToDp(context: Context, pixels: Int) =
-    (pixels / context.resources.displayMetrics.density).toInt()
