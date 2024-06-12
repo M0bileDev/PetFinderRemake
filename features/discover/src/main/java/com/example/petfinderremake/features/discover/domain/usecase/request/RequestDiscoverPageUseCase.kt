c670e0390febc21b9451085e2b215d600f320403
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-12 19:47:35 +0200

    Logic updated from Kotlin's reactive types to RxJava's reactive types;

--- features/discover/src/main/java/com/example/petfinderremake/features/discover/domain/usecase/request/RequestDiscoverPageUseCase.kt
+++ features/discover/src/main/java/com/example/petfinderremake/features/discover/domain/usecase/request/RequestDiscoverPageUseCase.kt
@@ -1,13 +1,11 @@
 package com.example.petfinderremake.features.discover.domain.usecase.request
 
-import com.example.petfinderremake.common.domain.model.pagination.PaginatedAnimals
 import com.example.petfinderremake.common.domain.repositories.AnimalRepository
 import com.example.petfinderremake.common.domain.result.Result
 import com.example.petfinderremake.common.domain.result.RootError
 import com.example.petfinderremake.common.domain.result.error.NetworkError
 import com.example.petfinderremake.common.domain.result.error.StorageError
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.HttpException
 import javax.inject.Inject
 
@@ -15,35 +13,38 @@ class RequestDiscoverPageUseCase @Inject constructor(
     private val animalRepository: AnimalRepository,
 ) {
 
-    suspend operator fun invoke(
+    operator fun invoke(
         onLoading: (Boolean) -> Unit,
-    ): Result<Unit, RootError> {
-        return withContext(Dispatchers.IO) {
-            onLoading(true)
-            val paginatedAnimals: PaginatedAnimals
+    ): Observable<Result<Unit, RootError>> {
 
-            try {
-                paginatedAnimals = animalRepository.requestDiscoverPage()
-            } catch (exception: HttpException) {
-                onLoading(false)
-                val networkError =
-                    NetworkError.entries.find { it.code == exception.code() }
+        return animalRepository.requestDiscoverPage()
+            .doOnSubscribe {
+                onLoading(true)
+            }
+            .flatMap { data ->
+                Observable.fromCallable<Result<Unit, RootError>> {
+                    when {
+                        data.animals.isEmpty() -> {
+                            Result.Error(StorageError.NO_DATA_TO_STORE)
+                        }
+
+                        else -> {
+                            animalRepository.storeDiscoverPaginatedAnimals(data)
+                            Result.Success(Unit)
+                        }
+                    }
+                }
+            }.onErrorResumeWith { error ->
+                if (error is HttpException) {
+                    val networkError = NetworkError.entries.find { it.code == error.code() }
                         ?: throw NetworkError.NetworkErrorTypeException()
-
-                return@withContext Result.Error(networkError)
+                    Result.Error(networkError)
+                } else {
+                    throw Exception()
+                }
             }
-
-            if (paginatedAnimals.animals.isEmpty()) {
+            .doOnNext {
                 onLoading(false)
-                return@withContext Result.Error(
-                    StorageError.NO_DATA_TO_STORE
-                )
             }
-
-            animalRepository.storeDiscoverPaginatedAnimals(paginatedAnimals)
-                .run { onLoading(false) }
-                .run { Result.Success(Unit) }
-
-        }
     }
 }
