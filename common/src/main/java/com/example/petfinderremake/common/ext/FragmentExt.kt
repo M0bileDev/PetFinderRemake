52bbc21a3729dcd420a1c3c35e286af21e778bab
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-12 19:40:40 +0200

    Logic updated from Kotlin's reactive types to RxJava's reactive types;

--- common/src/main/java/com/example/petfinderremake/common/ext/FragmentExt.kt
+++ common/src/main/java/com/example/petfinderremake/common/ext/FragmentExt.kt
@@ -2,9 +2,22 @@ package com.example.petfinderremake.common.ext
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
-fun Fragment.getNavigationResult(key: String = "result") =
-    findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<String>(key) ?: throw Exception("getNavigationResult error")
+fun Fragment.getNavigationResult(key: String = "result"): Observable<String> {
+    val subject = BehaviorSubject.create<String>()
+    val saveStateHandle = findNavController().currentBackStackEntry?.savedStateHandle
+    val liveData =
+        saveStateHandle?.getLiveData<String>(key) ?: throw Exception("getNavigationResult error")
+    liveData.observe(this) { result ->
+        result?.let {
+            subject.onNext(it)
+        }
+    }
+    return subject.hide()
+}
 
 fun Fragment.setNavigationResult(result: String, key: String = "result") =
-    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, result) ?: throw Exception("setNavigationResult error")
+    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, result)
+        ?: throw Exception("setNavigationResult error")
