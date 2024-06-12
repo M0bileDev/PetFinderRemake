04aa814085b23c7168f5b4269af0985a9b379b77
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-12 19:50:40 +0200

    Methods return type was changed from Kotlin's Flow to RxJava's Observable.

--- features/search/src/main/java/com/example/petfinderremake/features/search/presentation/navigation/SearchNavigation.kt
+++ features/search/src/main/java/com/example/petfinderremake/features/search/presentation/navigation/SearchNavigation.kt
@@ -2,12 +2,12 @@ package com.example.petfinderremake.features.search.presentation.navigation
 
 import androidx.fragment.app.Fragment
 import com.example.petfinderremake.common.domain.model.AnimalParameters
-import kotlinx.coroutines.flow.Flow
+import io.reactivex.rxjava3.core.Observable
 
 interface SearchNavigation {
     fun navigateToFilter(fragment: Fragment, filterNavArg: AnimalParameters)
     fun getFilterNavArg(fragment: Fragment): AnimalParameters
     fun clearFilterNavArgs(fragment: Fragment)
-    fun observeResultNavArg(fragment: Fragment): Flow<AnimalParameters>
+    fun observeResultNavArg(fragment: Fragment): Observable<AnimalParameters>
     fun clearResultNavArg(fragment: Fragment)
 }
