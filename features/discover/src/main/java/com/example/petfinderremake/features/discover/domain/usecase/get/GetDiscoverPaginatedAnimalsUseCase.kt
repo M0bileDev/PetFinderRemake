21c1608e377ee880106abb2db80d7a02ae5f832d
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-12 19:47:12 +0200

    Methods return type was changed from Kotlin's Flow to RxJava's Observable.

--- features/discover/src/main/java/com/example/petfinderremake/features/discover/domain/usecase/get/GetDiscoverPaginatedAnimalsUseCase.kt
+++ features/discover/src/main/java/com/example/petfinderremake/features/discover/domain/usecase/get/GetDiscoverPaginatedAnimalsUseCase.kt
@@ -4,17 +4,18 @@ import com.example.petfinderremake.common.domain.model.pagination.PaginatedAnima
 import com.example.petfinderremake.common.domain.repositories.AnimalRepository
 import com.example.petfinderremake.common.domain.result.NotYetDefinedError
 import com.example.petfinderremake.common.domain.result.Result
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
 class GetDiscoverPaginatedAnimalsUseCase @Inject constructor(
     private val animalRepository: AnimalRepository
 ) {
 
-    operator fun invoke(): Flow<Result<PaginatedAnimals, NotYetDefinedError>> {
-        return animalRepository.getDiscoverPaginatedAnimals().map { data ->
-            Result.Success(data)
-        }
+    operator fun invoke(): Observable<Result<PaginatedAnimals, NotYetDefinedError>> {
+        return animalRepository
+            .getDiscoverPaginatedAnimals()
+            .map { data ->
+                Result.Success(data)
+            }
     }
 }
