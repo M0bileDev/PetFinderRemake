22019e5c723f3d9410428b39d5dd9444af3efc6a
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-19 22:04:46 +0200

    Tests structures updates;

--- features/details/animals/src/test/java/com/example/petfinderremake/features/details/animals/domain/GetAnimalUseCaseTest.kt
+++ features/details/animals/src/test/java/com/example/petfinderremake/features/details/animals/domain/GetAnimalUseCaseTest.kt
@@ -1,11 +1,11 @@
 package com.example.petfinderremake.features.details.animals.domain
 
 import com.example.petfinderremake.common.domain.model.animal.details.AnimalWithDetails
+import com.example.petfinderremake.common.domain.result.NotYetDefinedError
 import com.example.petfinderremake.common.domain.result.Result
 import com.example.petfinderremake.features.details.animals.AnimalRepositoryTest
 import com.google.common.truth.Truth
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import io.reactivex.rxjava3.observers.TestObserver
 import org.junit.Before
 import org.junit.Test
 
@@ -19,48 +19,42 @@ class GetAnimalUseCaseTest : AnimalRepositoryTest() {
     @Before
     fun setup() {
         getAnimalUseCase = GetAnimalUseCase(animalRepository)
-
-        runBlocking {
-            animalRepository.storeAnimals(animals)
-        }
+        animalRepository.storeAnimals(animals)
     }
 
     @Test
     fun `when get animal, then result of use case is instance of Result`() {
-        runBlocking {
+        //when
+        val testObserver = TestObserver<Result<AnimalWithDetails, NotYetDefinedError>>()
+        val result = getAnimalUseCase(animal.id)
+        result.subscribe(testObserver)
 
-            //when
-            val result = getAnimalUseCase(animal.id).first()
-
-            //then
-            Truth.assertThat(result).isInstanceOf(Result::class.java)
-
-        }
+        //then
+        val sut = testObserver.values().first()
+        Truth.assertThat(sut).isInstanceOf(Result::class.java)
     }
 
     @Test
     fun `when get animal, then result of use case is instance of Result Success`() {
-        runBlocking {
+        //when
+        val testObserver = TestObserver<Result<AnimalWithDetails, NotYetDefinedError>>()
+        val result = getAnimalUseCase(animal.id)
+        result.subscribe(testObserver)
 
-            //when
-            val result = getAnimalUseCase(animal.id).first()
-
-            //then
-            Truth.assertThat(result).isInstanceOf(Result.Success::class.java)
-
-        }
+        //then
+        val sut = testObserver.values().first()
+        Truth.assertThat(sut).isInstanceOf(Result.Success::class.java)
     }
 
     @Test
     fun `when get animal with previously set value, then result of type Result Success is the same value`() {
-        runBlocking {
+        //when
+        val testObserver = TestObserver<Result<AnimalWithDetails, NotYetDefinedError>>()
+        val result = getAnimalUseCase(animal.id)
+        result.subscribe(testObserver)
 
-            //when
-            val result = getAnimalUseCase(animal.id).first()
-
-            //then
-            Truth.assertThat(result).isEqualTo(Result.Success(animal))
-
-        }
+        //then
+        val sut = testObserver.values().first()
+        Truth.assertThat(sut).isEqualTo(Result.Success(animal))
     }
 }
