618e742cf9c27a0b50e2c6c1dfa64b9be0420a72
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-19 22:08:58 +0200

    Test resources updates;

--- features/filter/src/androidTest/java/com/example/petfinderremake/features/filter/presentation/navigation/TestPetFinderFilterNavigation.kt
+++ features/filter/src/androidTest/java/com/example/petfinderremake/features/filter/presentation/navigation/TestPetFinderFilterNavigation.kt
@@ -3,8 +3,9 @@ package com.example.petfinderremake.features.filter.presentation.navigation
 import androidx.fragment.app.Fragment
 import com.example.petfinderremake.common.domain.model.AnimalParameters
 import com.example.petfinderremake.features.filter.presentation.model.navigation.SelectNavArg
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 class TestPetFinderFilterNavigation @Inject constructor() : FilterNavigation {
@@ -17,8 +18,10 @@ class TestPetFinderFilterNavigation @Inject constructor() : FilterNavigation {
     override fun clearFilterNavArgs(fragment: Fragment) {
     }
 
-    override fun observeResultNavArg(fragment: Fragment): Flow<AnimalParameters> {
-        return flow { }
+    override fun observeResultNavArg(fragment: Fragment): Observable<AnimalParameters> {
+        return Observable.just(AnimalParameters.noAnimalParameters)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
 
     override fun clearResultNavArg(fragment: Fragment) {
