fe55f3a482276d99df80d4fb32b310130751a778
m0bileDev <damian.ogorek.1996@gmail.com>
2024-06-19 22:00:35 +0200

    Test resources updates;

--- common/src/test/java/com/example/petfinderremake/common/data/repositories/FakeNotificationRepository.kt
+++ common/src/test/java/com/example/petfinderremake/common/data/repositories/FakeNotificationRepository.kt
@@ -2,37 +2,43 @@ package com.example.petfinderremake.common.data.repositories
 
 import com.example.petfinderremake.common.domain.model.notification.Notification
 import com.example.petfinderremake.common.domain.repositories.NotificationRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
 
 class FakeNotificationRepository : NotificationRepository {
 
     private val notifications = mutableListOf<Notification>()
 
-    override suspend fun insert(notification: Notification) {
+    override
+    fun insert(notification: Notification): Completable {
         notifications.add(notification)
+        return Completable.complete()
     }
 
-    override suspend fun update(notification: Notification) {
+    override
+    fun update(notification: Notification): Completable {
         val notificationToUpdate = notifications.first { it.id == notification.id }
         val index = notifications.indexOf(notificationToUpdate)
         notifications[index] = notification
+        return Completable.complete()
     }
 
-    override suspend fun delete(notification: Notification) {
+    override
+    fun delete(notification: Notification): Completable {
         notifications.remove(notification)
+        return Completable.complete()
     }
 
-    override fun getAllNotifications(): Flow<List<Notification>> {
-        return flow { emit(notifications) }
+    override fun getAllNotifications(): Observable<List<Notification>> {
+        return Observable.just(notifications)
     }
 
-    override fun getNotDisplayedNotifications(): Flow<List<Notification>> {
-        return flow { emit(notifications.filter { !it.displayed }) }
+    override fun getNotDisplayedNotifications(): Observable<List<Notification>> {
+        return Observable.just(notifications).map { it.filter { !it.displayed } }
     }
 
-    override fun getSingleNotification(id: Long): Flow<Notification> {
-        return flow { emit(notifications.first { it.id == id }) }
+    override fun getSingleNotification(id: Long): Observable<Notification> {
+        return Observable.just(notifications).map { it.first { it.id == id } }
     }
 
 }
